f50109b2eb7aba463124ae5be76a38f6a177e1ee
Nitin Sharma <nitin.sharma@ens.enterprises>
2024-10-18 13:05:52 +0530

    initial commit

---
+++ my-headless-shopify-store/src/app/pages/home.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+      <div>
+        <h1 className="text-4xl font-bold">Welcome to My Headless CMS!</h1>
+      </div>
+    );
+  };
+  
+  export default HomePage;
---
+++ my-headless-shopify-store/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Adjust according to your folder structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
